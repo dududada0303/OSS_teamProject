8fe6c6a5716367de2d55361321c691738a40215e
hwayeong-2 <hhhyeong222@gmail.com>
2026-05-25 12:07:32 +0900

    카메라 고정 취소

--- Assets/Scripts/Camera/TrackingPlayerCamera.cs
+++ Assets/Scripts/Camera/TrackingPlayerCamera.cs
@@ -2,12 +2,18 @@ using UnityEngine;
 
 public class TrackingPlayerCamera : MonoBehaviour
 {
+    // 플레이어의 transform 컴포넌트
     public Transform Player;
+    // 플레이어와 카메라 사이 항상 존재해야하는 거리 값
     public float distanceBetween;
 
+    // 플레이어가 움직인 후 카메라가 따라가야 하므로, LateUpdate() 메서드 이용
     private void LateUpdate()
     {
-        // 플레이어를 따라가는 코드를 모두 지웠습니다.
-        // 이제 카메라는 유니티 씬 뷰에 배치해 둔 그 자리에 완전히 고정됩니다.
+        // 카메라의 현재 위치
+        Vector3 cameraPos = transform.position;
+        // 카메라의 위치(y 좌표만)를 플레이어 y 좌표에서 특정 거리만큼 떨어진 위치로 이동시키는 부분
+        cameraPos.y = Player.position.y + distanceBetween;
+        transform.position = cameraPos;
     }
 }
